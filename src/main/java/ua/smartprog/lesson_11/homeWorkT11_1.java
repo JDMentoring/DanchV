5c1569537d3150072e0a5e2a06a529c987ddd106
NAzarMh <nazar.myhailiv@gmail.com>
2016-12-26 22:20:38 +0200

    Simple package work

--- src/main/java/ua/smartprog/lesson_11/homeWorkT11_1.java
+++ src/main/java/ua/smartprog/lesson_11/homeWorkT11_1.java
@@ -1,8 +1,5 @@
 package ua.smartprog.lesson_11;
 
-/**
- * Created by NazarMh on 26.12.2016.
- */
 public class homeWorkT11_1 {
     public static void main(String[] args) {
         System.out.println("Solving task");
