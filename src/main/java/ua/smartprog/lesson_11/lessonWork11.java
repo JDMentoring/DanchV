62870db0629d452a7d73f3233459196182c42d02
NAzarMh <nazar.myhailiv@gmail.com>
2016-12-26 21:27:59 +0200

    some test

--- src/main/java/ua/smartprog/lesson_11/lessonWork11.java
+++ src/main/java/ua/smartprog/lesson_11/lessonWork11.java
@@ -4,5 +4,7 @@ package ua.smartprog.lesson_11;
 public class lessonWork11 {
     public static void main(String[] args) {
         System.out.println("New package");
+        System.out.println("New line");
+
     }
 }
