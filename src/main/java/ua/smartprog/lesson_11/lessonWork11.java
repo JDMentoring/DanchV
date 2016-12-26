7190003480a5308348aeba81d43a00ed299e1565
NAzarMh <nazar.myhailiv@gmail.com>
2016-12-26 21:27:37 +0200

    some test

--- src/main/java/ua/smartprog/lesson_11/lessonWork11.java
+++ src/main/java/ua/smartprog/lesson_11/lessonWork11.java
@@ -1,8 +1,6 @@
 package ua.smartprog.lesson_11;
 
-/**
- * Created by NazarMh on 26.12.2016.
- */
+
 public class lessonWork11 {
     public static void main(String[] args) {
         System.out.println("New package");
