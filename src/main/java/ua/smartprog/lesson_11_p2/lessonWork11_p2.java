6615500fc426b60ef6c0d0c573cc237b43ae09f4
NAzarMh <nazar.myhailiv@gmail.com>
2016-12-26 21:43:17 +0200

    Test package work

--- src/main/java/ua/smartprog/lesson_11_p2/lessonWork11_p2.java
+++ src/main/java/ua/smartprog/lesson_11_p2/lessonWork11_p2.java
@@ -1,9 +1,9 @@
-package ua.smartprog.lesson_11;
+package ua.smartprog.lesson_11_p2;
 
 /*
 * Home work
 * */
-public class lessonWork11 {
+public class lessonWork11_p2 {
     public static void main(String[] args) {
         System.out.println("New package");
         System.out.println("New line");
