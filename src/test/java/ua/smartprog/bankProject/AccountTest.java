1b0f8145960654ea19bf42f4483e84b66412430b
NAzarMh <nazar.myhailiv@gmail.com>
2017-01-21 19:57:36 +0200

    Test for Task 1502

---
+++ src/test/java/ua/smartprog/bankProject/AccountTest.java
@@ -0,0 +1,13 @@
+package ua.smartprog.bankProject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccountTest {
+    @Test // Task 1502
+    public void generateCardNumberTest() throws Exception {
+        Account ac = new Account();
+        assertEquals(16, ac.generateCardNumber().length());
+    }
+}
