9b3355db9ca46c87f4c1e890f7cf69e9cec25cf7
NAzarMh <nazar.myhailiv@gmail.com>
2017-01-23 19:37:24 +0200

    Check TODO comments!!!

--- src/test/java/ua/smartprog/bankProject/AccountTest.java
+++ src/test/java/ua/smartprog/bankProject/AccountTest.java
@@ -8,6 +8,7 @@ public class AccountTest {
     @Test // Task 1502
     public void generateCardNumberTest() throws Exception {
         Account ac = new Account();
+        //TODO Реалізувати метод із сигнатурою наведеною нижче
         assertEquals(16, ac.generateCardNumber().length());
     }
 }
