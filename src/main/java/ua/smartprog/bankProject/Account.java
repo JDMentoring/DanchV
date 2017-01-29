1c2207463b167655b4bd35826ccb45bff4a9d6db
dan4ikV <danylo.wasylyshyn@gmail.com>
2017-01-29 12:22:34 +0200

    stuck on 1601

--- src/main/java/ua/smartprog/bankProject/Account.java
+++ src/main/java/ua/smartprog/bankProject/Account.java
@@ -4,12 +4,15 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Account {
+    Random generator = new Random();
+    private static final int PASSLEN = 8;
     private int balance;
     private String cardNumber;
     private String password;
 
     public Account(){
         this.cardNumber = generateCardNumber();
+        generateDefaultPassword();
     }
 
 
@@ -48,10 +51,9 @@ public class Account {
     }
 
     public String generateVCNumber(){
-        Random rand = new Random();
         StringBuilder num = new StringBuilder();
         for (int i = 0; i < num.capacity(); i++) {
-            int temp = rand.nextInt(10) ;
+            int temp = generator.nextInt(10) ;
             if(i == num.length()){
                 temp = 4;
             }
@@ -81,6 +83,43 @@ public class Account {
 
     //start 1601
     //start 1601
+
+    public void generateDefaultPassword(){
+        int[] passarray = new int[PASSLEN];
+        int check1 = 0;
+        int check2 = 0;
+        int check3 = 0;
+        int diapason;
+        for (int i = 0; i < PASSLEN; i++) {
+            diapason = generator.nextInt(2);
+            if(diapason == 0){
+                passarray[i] = (char)95;
+                if(check1 == 0){
+                    check1 = 1;
+                }
+            }
+            else if(diapason == 1){
+                passarray[i] = (char)generator.nextInt(122) + 97;
+                if(check2 == 0){
+                    check2 = 1;
+                }
+            }
+            else if(diapason == 2){
+                passarray[i] = (char)generator.nextInt(48) + 57;
+                if(check3 == 0){
+                    check3 = 1;
+                }
+            }
+        }
+        System.out.println(passarray.toString());
+        if(check1 == 1 && check2 == 1 && check3 == 1){
+            this.password = passarray.toString();
+        }
+        else{
+            generateDefaultPassword();
+        }
+    }
+    //Dont know why is the mistake happening
 
 
     public String getCardNumber() {
