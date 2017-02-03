7a2f0427dda0b9f22a99a97bd593efe77403a7a9
dan4ikV <danylo.wasylyshyn@gmail.com>
2017-02-03 20:53:02 +0200

    start refactr

--- src/main/java/ua/smartprog/bankProject/Customer.java
+++ src/main/java/ua/smartprog/bankProject/Customer.java
@@ -2,47 +2,43 @@ package ua.smartprog.bankProject;
 
 public class Customer extends Human {
     private Account[] CustomerAccount;
+    int account_num = 0;
     private int PhoneNumber;
 
     public Customer() {
         PhoneNumber = 0;
-        this.CustomerAccount = new Account();
-        CustomerAccount.setMoney(20);
-        CustomerAccount.generateCardNumber();
-        CustomerAccount.generateVCNumber();
+        this.CustomerAccount = new Account[account_num + 1];
+        CustomerAccount[account_num].addMoney(20);
+        CustomerAccount[account_num].generateCardNumber();
+        CustomerAccount[account_num].generateVCNumber();
         this.CustomerAccount = new Account[1];
         Account defaultAccount = new Account();
         CustomerAccount[0] = defaultAccount;
+        System.out.println("It's account number " + (account_num + 1));
     }
-
-    //start refactoring
 
     public Customer(String fName, String sName, int cAge, int phoneNumber, int Money) {
         super(fName, sName, cAge);
         PhoneNumber = phoneNumber;
-        this.CustomerAccount = new Account();
-        CustomerAccount.setMoney(Money);
-        CustomerAccount.generateCardNumber();
-        CustomerAccount.generateVCNumber();
+        this.CustomerAccount = new Account[account_num + 1];
+        CustomerAccount[account_num].addMoney(Money);
+        CustomerAccount[account_num].generateCardNumber();
+        CustomerAccount[account_num].generateVCNumber();
     }
 
     public void setPhoneNumber(int phoneNumber) {
         PhoneNumber = phoneNumber;
     }
 
-    public void setMoney(int ammount){
-        CustomerAccount.setMoney(ammount);
+    public void setMoney(int ammount, int numOfAcc){
+        CustomerAccount[numOfAcc].addMoney(ammount);
     }
 
-    public int getMoney(){
-        return CustomerAccount.checkBalance();
+    public int getMoney(int numOfAcc){
+        return CustomerAccount[numOfAcc].checkBalance();
     }
-    public void CusTransaction(int i, Customer num2){
-        setMoney(getMoney() - i);
-        num2.setMoney(num2.getMoney() + i);
-    }
-
-    public int getPhoneNumber() {
-        return PhoneNumber;
+    public void CusTransaction(int ammount, Account num2){
+        CustomerAccount[account_num].takeMoney(ammount);
+        num2.addMoney(ammount);
     }
 }
