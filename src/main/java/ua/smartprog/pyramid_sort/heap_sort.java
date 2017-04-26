1cb083a9086c3cadfd5a0e9f75a7ceed2f40e438
dan4ikV <danylo.wasylyshyn@gmail.com>
2017-04-26 21:58:15 +0300

    sort

--- src/main/java/ua/smartprog/pyramid_sort/heap_sort.java
+++ src/main/java/ua/smartprog/pyramid_sort/heap_sort.java
@@ -3,8 +3,47 @@ package ua.smartprog.pyramid_sort;
 /**
  * Created by Тарас on 26.04.2017.
  */
-public class heap_sort<T> {
-    public void sort(T[] array){
-        
+public class heap_sort<T extends Number> {
+    public void sort(T[] array) {
+        int len = array.length;
+        makeHeap(array);
+        while (len > 0) {
+            swap(array, 0, len - 1);
+            len--;
+            makeSubHeap(array, len, 0);
+        }
     }
+
+    public void makeSubHeap(T[] arrayToSubHeap, int length, int place) {
+        while (2 * place + 1 < length) {
+            int temp = 2 * place + 1;
+
+            if (2 * place + 2 < length && arrayToSubHeap[2 * place + 1] < arrayToSubHeap[2 * place + 2]) {
+                temp = 2 * place + 2;
+            }
+
+            if (arrayToSubHeap[place] < arrayToSubHeap[temp]) {
+                swap(arrayToSubHeap, temp, place);
+            } else {
+                break;
+            }
+        }
+        ;
+    }
+
+    public T[] makeHeap(T[] arrayToHeap) {
+        int len = arrayToHeap.length;
+        for (int i = len - 1; i >= 0; i--) {
+            makeSubHeap(arrayToHeap, len, i);
+        }
+        return arrayToHeap;
+    }
+
+    public void swap(T[] array, int pos1, int pos2) {
+        T temp = array[pos1];
+        array[pos1] = array[pos2];
+        array[pos2] = temp;
+    }
+
+
 }
---
+++ src/main/java/ua/smartprog/pyramid_sort/runsort.java
@@ -0,0 +1,16 @@
+package ua.smartprog.pyramid_sort;
+
+/**
+ * Created by Тарас on 26.04.2017.
+ */
+public class runsort {
+    public static void main(String[] args) {
+        double[] dArray = new double[20];
+        for (int i = 0; i < dArray.length; i++){
+            int n = 23;
+            dArray[i] = n + 40;
+            n--;
+        }
+        heap_sort
+    }
+}
