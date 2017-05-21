11fca62aa9803f666a2b32d5767cd6c716e6e77a
dan4ikV <danylo.wasylyshyn@gmail.com>
2017-05-21 10:25:27 +0300

    finish

--- src/test/java/ua/smartprog/bankProject/dao/AccountDaoTest.java
+++ src/test/java/ua/smartprog/bankProject/dao/AccountDaoTest.java
@@ -3,8 +3,12 @@ package ua.smartprog.bankProject.dao;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import ua.smartprog.bankProject.domain.Account;
 import ua.smartprog.bankProject.mysql.MySqlAccountDao;
 import ua.smartprog.bankProject.mysql.MySqlDaoFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 /**
  * Created by Тарас on 10.05.2017.
@@ -19,7 +23,13 @@ public class AccountDaoTest {
     @Test
     public void prepareStInsertTest() throws Exception{
         MySqlDaoFactory databaseFactory = new MySqlDaoFactory();
+        Connection con = databaseFactory.getConnection();
         MySqlAccountDao accountDao = new MySqlAccountDao(databaseFactory.getConnection());
-        accountDao.
-    }
-}
+        Account ac = new Account("1234123412341234", 1234, "admin1234");
+        String statement = "INSERT INTO Accounts (id, cardNumber, password, balance, cardPrint, cardEnd)" +
+                        "VALUES (?, ?, ?, ?, ?, ?);";
+        PreparedStatement prSt = con.prepareStatement(statement);
+        PreparedStatement prSt2 = con.prepareStatement(statement);
+        accountDao.prepareStInsert(prSt2, ac);
+        assertNotEquals(prSt, prSt2);
+};
