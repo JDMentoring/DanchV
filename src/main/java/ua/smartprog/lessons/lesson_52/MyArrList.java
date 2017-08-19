d5ca03cbff99a25bfeb1a738f6612aace0f95ec9
dan4ikV <danylo.wasylyshyn@gmail.com>
2017-08-19 10:57:25 +0300

    stop

--- src/main/java/ua/smartprog/lessons/lesson_52/MyArrList.java
+++ src/main/java/ua/smartprog/lessons/lesson_52/MyArrList.java
@@ -1,7 +1,14 @@
 package ua.smartprog.lessons.lesson_52;
 
+import java.util.List;
+
 /**
  * Created by Тарас on 19.08.2017.
  */
-public class MyArrList {
+public class MyArrList<T> {
+    private List<Node>;
+
+    public void add(T obj){
+        Node[0] =
+    }
 }
---
+++ src/main/java/ua/smartprog/lessons/lesson_52/Node.java
@@ -0,0 +1,13 @@
+package ua.smartprog.lessons.lesson_52;
+
+/**
+ * Created by Тарас on 19.08.2017.
+ */
+public class Node<T> {
+    private T object;
+    private Node pointer;
+
+    public void setPointer(Node pointer){
+        this.pointer = pointer;
+    }
+}
